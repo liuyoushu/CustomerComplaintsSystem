e182f125973c2e0e0f788e3d7db620ac43bec87a
刘悠舒 <liuyoushu@live.com>
2014-03-17 11:19:11 +0800

    initialize IDAL in Model.Repositories layer

---
+++ Code/CustomerComplaintsSystem/Neusoft.CCS.Model/Repositories/IDAL.cs
@@ -0,0 +1,65 @@
+﻿
+ 
+ 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Neusoft.CCS.Model.Entities;
+
+namespace Neusoft.CCS.Model.Repositories
+{
+	public partial interface IBusinessDAL : IBaseRepository<MODEL.Business>
+    {
+    }
+
+	public partial interface ICaseInfoDAL : IBaseRepository<MODEL.CaseInfo>
+    {
+    }
+
+	public partial interface ICompanyDAL : IBaseRepository<MODEL.Company>
+    {
+    }
+
+	public partial interface IComplainerDAL : IBaseRepository<MODEL.Complainer>
+    {
+    }
+
+	public partial interface IComplaintDisposeAndFeedbackInfoDAL : IBaseRepository<MODEL.ComplaintDisposeAndFeedbackInfo>
+    {
+    }
+
+	public partial interface IComplaintInfoDAL : IBaseRepository<MODEL.ComplaintInfo>
+    {
+    }
+
+	public partial interface IComplaintReturnVisitInfoDAL : IBaseRepository<MODEL.ComplaintReturnVisitInfo>
+    {
+    }
+
+	public partial interface IDepartmentDAL : IBaseRepository<MODEL.Department>
+    {
+    }
+
+	public partial interface IImportantEvent_CenterDAL : IBaseRepository<MODEL.ImportantEvent_Center>
+    {
+    }
+
+	public partial interface IImportantEvent_DepartmentDAL : IBaseRepository<MODEL.ImportantEvent_Department>
+    {
+    }
+
+	public partial interface IImportantEvent_StaffDAL : IBaseRepository<MODEL.ImportantEvent_Staff>
+    {
+    }
+
+	public partial interface IPositionDAL : IBaseRepository<MODEL.Position>
+    {
+    }
+
+	public partial interface IStaffDAL : IBaseRepository<MODEL.Staff>
+    {
+    }
+
+
+}
