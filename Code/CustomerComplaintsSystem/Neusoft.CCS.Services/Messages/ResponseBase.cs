55435f19c7f7f437d3a5dc371f8bc2466834d1c6
刘悠舒 <liuyoushu@live.com>
2014-04-04 21:17:39 +0800

    Added ResponseBase in Service layer.

---
+++ Code/CustomerComplaintsSystem/Neusoft.CCS.Services/Messages/ResponseBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neusoft.CCS.Services.Messages
+{
+    public class ResponseBase
+    {
+        public bool IsSuccess { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
