59829682bd736d0b2d21153b78e11b1a7182b4a8
刘悠舒 <liuyoushu@live.com>
2014-04-08 14:35:15 +0800

    Initialized the subsystem areas

---
+++ Code/CustomerComplaintsSystem/Neusoft.CCS.WebUI/Areas/CHS/CHSAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Neusoft.CCS.WebUI.Areas.CHS
+{
+    public class CHSAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "CHS";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "CHS_default",
+                "CHS/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Code/CustomerComplaintsSystem/Neusoft.CCS.WebUI/Areas/RVASS/RVASSAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Neusoft.CCS.WebUI.Areas.RVASS
+{
+    public class RVASSAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "RVASS";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "RVASS_default",
+                "RVASS/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
